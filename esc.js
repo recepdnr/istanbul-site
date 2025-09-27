b814bb12364b1469bdea967e8d1fa4bd26ada7f4
recepdnr <recepdnrdnr@gmail.com>
2025-09-27 14:09:33 +0300

    Add esc.js

---
+++ esc.js
@@ -0,0 +1,22 @@
+const partnersData = {
+  1: {
+    adsoyad: "kral kral",
+    yaş: "23",
+    boy: "232",
+    cinsiyet: "kadın",
+    şehir: "istanbul",
+    açıklama: "kral asalan",
+    phoneNumber: "+905347793580",
+    folderNumber: "1",
+  },
+  2: {
+    adsoyad: "kral adam",
+    yaş: "24",
+    boy: "242",
+    cinsiyet: "kadın",
+    şehir: "istanbul",
+    açıklama: "kral adam maxc",
+    phoneNumber: "+905347793580",
+    folderNumber: "2",
+  },
+};
