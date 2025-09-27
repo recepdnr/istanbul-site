3b1735b96c0d1eb8b98eadbd674a82257e0f3027
recepdnr <recepdnrdnr@gmail.com>
2025-09-27 17:16:23 +0300

    Add esc.js

---
+++ esc.js
@@ -0,0 +1,12 @@
+const partnersData = {
+  1: {
+    adsoyad: "ad soyad",
+    yaş: "34",
+    boy: "454",
+    cinsiyet: "kadın",
+    şehir: "istanbul",
+    açıklama: "5347793580",
+    phoneNumber: "+905347793580",
+    folderNumber: "1",
+  },
+};
