5a5088c0de444aac9bb5706d18281778bdd99d3e
recepdnr <recepdnrdnr@gmail.com>
2025-10-02 14:12:14 +0300

    Add esc.js

---
+++ esc.js
@@ -0,0 +1,12 @@
+const partnersData = {
+  1: {
+    adsoyad: "aslan max",
+    yaş: "34",
+    boy: "160",
+    cinsiyet: "erkek",
+    şehir: "istanbul",
+    açıklama: "5347793580",
+    phoneNumber: "+905347793580",
+    folderNumber: "1",
+  },
+};
